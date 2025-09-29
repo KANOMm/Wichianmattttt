52289e2aed623ab04148b81f64553d849e650b8a
NopNopNoppppp <100502815+KANOMm@users.noreply.github.com>
2025-09-29 19:33:23 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const fetch = require('node-fetch');  // ใช้ v2.x เท่านั้น
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.post('/homework', async (req, res) => {
+  try {
+    const googleScriptUrl = 'https://script.google.com/macros/s/AKfycbx668NsdRR9YxhqQvULobg_NgrUByeerzk6in9jO-TfqX_IZ9qNYUxshGwAK3V0yoUa8w/exec';
+
+    const response = await fetch(googleScriptUrl, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(req.body)
+    });
+
+    const data = await response.json();
+
+    res.json(data);
+
+  } catch (error) {
+    res.status(500).json({ status: 'error', message: error.message });
+  }
+});
+
+app.listen(3000, () => {
+  console.log('Server running on http://localhost:3000');
+});
